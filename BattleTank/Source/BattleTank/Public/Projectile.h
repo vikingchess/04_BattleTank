960ad0c962e3ca060af51add26e1c938b139ad5d
DESKTOP-O8GP7PU\David <davidflom01@gmail.com>
2017-08-05 07:50:04 -0400

    end of video 191 working

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -22,8 +22,6 @@ protected:
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 	void LaunchProjectile(float Speed);
 
@@ -36,5 +34,12 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UParticleSystemComponent* LaunchBlast = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UParticleSystemComponent* ImpactBlast = nullptr;
+
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
 	
 };
